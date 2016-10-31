27972568519cfe249e98194ead752669278df583
Muhammad Abdurrahman Lathief Pratama <Muhammad Abdurrahman Lathief Pratama>
2016-10-31 15:43:29 +0700

    Initial commit

---
+++ app/src/main/java/id/rahmanlathief/newapp/MainActivity.java
@@ -0,0 +1,36 @@
+package id.rahmanlathief.newapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar seek = (SeekBar) findViewById(R.id.seeker);
+        final TextView svalue = (TextView) findViewById(R.id.seekVal);
+
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                svalue.setText(String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+}
